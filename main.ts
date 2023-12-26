abf8b899e703b4b796a5df1640460fca76ccc2ef
Polokego <pomakga022@student.wethinkcode.co.za>
2023-12-26 20:57:36 +0200

    Add sky n floor | move this thing quickly

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
 import * as Three from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';// Set up scene
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import {STLLoader} from 'three/examples/jsm/loaders/STLLoader';
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader';
 
 //const dinoModel = new URL("../Piko/Assets/T_Rex/source/dinosaur.glb", import.meta.url);
+
 const modelLoader = new GLTFLoader();
-let mixer, bracioMixer;
+const objLoader = new OBJLoader();
+const stlLoader = new STLLoader();
+const matLoader = new MTLLoader();
+let birdModel;
+let rexMixer, bracioMixer, birdMixer, pteraMixer;
 const scene = new Three.Scene();
 
 modelLoader.load("/Assets/T_Rex/source/dinosaur.glb", (gltf) => {
@@ -13,9 +21,9 @@ modelLoader.load("/Assets/T_Rex/source/dinosaur.glb", (gltf) => {
       gltf.scene.scale.set(10,10,10);
       gltf.scene.position.add(new Three.Vector3(-20,0,-20));
         scene.add(gltf.scene);
-        mixer = new Three.AnimationMixer(gltf.scene);
+        rexMixer = new Three.AnimationMixer(gltf.scene);
         var animation = gltf.animations[0];
-        var action = mixer.clipAction(animation);
+        var action = rexMixer.clipAction(animation);
         action.play();
   
 })
@@ -23,7 +31,7 @@ modelLoader.load("/Assets/T_Rex/source/dinosaur.glb", (gltf) => {
 modelLoader.load("/Assets/braciosaurus.glb", (gltf) => {
 
       
-  gltf.scene.scale.set(10,10,10);
+  gltf.scene.scale.set(11,11,11);
   gltf.scene.position.add(new Three.Vector3(20,0,-20));
     scene.add(gltf.scene);
     bracioMixer = new Three.AnimationMixer(gltf.scene);
@@ -32,6 +40,141 @@ modelLoader.load("/Assets/braciosaurus.glb", (gltf) => {
     action.play();
 
 })
+
+
+modelLoader.load("/Assets/bird2.glb", (gltf) => {
+
+      
+    gltf.scene.scale.set(0.3,0.3,0.3);
+    gltf.scene.rotation.y += 0.4;
+    gltf.scene.position.add(new Three.Vector3(-40,30,-20));
+    scene.add(gltf.scene);
+    birdModel = gltf.scene; // Store the model reference
+
+    // If you want to set an initial rotation
+    birdModel.rotation.set(0, Math.PI *1.5, 0);
+    birdMixer = new Three.AnimationMixer(gltf.scene);
+    var animation = gltf.animations[0];
+    var action = birdMixer.clipAction(animation);
+    action.play();
+
+})
+
+
+modelLoader.load("/Assets/pine_tree.glb", (gltf) => {
+
+      
+  gltf.scene.scale.set(0.3,0.3,0.3);
+  gltf.scene.position.add(new Three.Vector3(60,0,-20));
+  scene.add(gltf.scene);
+
+})
+/*
+objLoader.load("/Assets/polokego/polokego.obj",(obj)=>{
+  //obj.scale.set(3,3,3);
+  obj.position.add(new Three.Vector3(40,0,-40));
+  scene.add(obj);
+  let max = 20
+  obj.traverse(function (child) {
+    if (child instanceof Three.Mesh) {
+      child.position.x = max;
+          max += 40;
+
+    }
+  });
+});
+*/
+matLoader.load("Assets/polokego/polokego.mtl",(matSrc)=>{
+  
+  
+})
+
+
+modelLoader.load("/Assets/lego_man_prop/scene.gltf  ", (gltf) => {
+
+      
+  gltf.scene.scale.set(3,3,3);
+  gltf.scene.position.add(new Three.Vector3(80,9,-20));/*
+  let max = 5
+  gltf.scene.traverse(function (child) {
+    if (child instanceof Three.Mesh) {
+      child.position.x = max;
+          max += 5;
+
+    }
+  });*/
+  scene.add(gltf.scene);
+
+})
+const geoCloud = new Three.BoxGeometry(20,5,40);
+const matCloud = new Three.MeshStandardMaterial({color: 0xFFFFFF});
+const cloud = new Three.InstancedMesh(geoCloud, matCloud, 100);
+
+scene.background = new Three.Color(0x87CEEB);
+const instanceObj = new Three.Object3D;
+const geoPlane = new Three.PlaneGeometry(400,400);
+const matPlane = new Three.MeshStandardMaterial({color: 0x76552b});
+const meshPlane  = new Three.Mesh(geoPlane, matPlane);
+meshPlane.rotation.x = -0.5*Math.PI;
+for (let k= 0; k< 20; k++) {
+
+  instanceObj.position.x = 150 - Math.round(300*Math.random());
+  instanceObj.position.y = 100 + Math.round(20*Math.random());
+  instanceObj.position.z = 100 - Math.round(200*Math.random());
+  instanceObj.updateMatrix();
+  cloud.setMatrixAt(k, instanceObj.matrix);
+
+}
+
+scene.add(meshPlane)
+scene.add(cloud);
+
+
+matLoader.load("/Assets/legoManModel/legoManModel.mtl", (matLego) => {
+
+  matLego.preload();
+  objLoader.setMaterials(matLego);
+  objLoader.load("/Assets/legoManModel/legoManModel.obj", (lego) => {
+
+    
+    lego.scale.set(0.4,0.4,0.4);
+    lego.position.add(new Three.Vector3(-60,0,-20));/*
+    let max = 5
+    gltf.scene.traverse(function (child) {
+      if (child instanceof Three.Mesh) {
+        child.position.x = max;
+            max += 5;
+
+      }
+    });*/
+    scene.add(lego);
+    console.log(lego)
+
+  })
+});
+/*
+
+objLoader.load("/Assets/Minifig/Minifig.obj",(obj)=>{
+  //obj.scale.set(3,3,3);
+  obj.position.add(new Three.Vector3(40,0,-40));
+  walkModel = new Three.AnimationMixer(obj);
+  var animation = obj.animations[0];
+  var action = walkModel.clipAction(animation);
+  action.play();
+  scene.add(obj);
+  let max = 20
+  obj.traverse(function (child) {
+    if (child instanceof Three.Mesh) {
+      child.position.x = max;
+          max += 40;
+
+    }
+  });
+});
+
+*/
+
+
 /**/
 const renderer = new Three.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -57,15 +200,35 @@ scene.add(ambientLight);
 scene.add(gridHelper);
 
 const controls = new OrbitControls(camera,renderer.domElement);
-
+  var birdTime = 0;
 
  function animation() {
-  if (mixer) {
-    mixer.update(0.024); // Assuming 60 FPS
+  if (rexMixer) {
+    rexMixer.update(0.024); // Assuming 60 FPS
   }  
   if (bracioMixer){
     bracioMixer.update(0.03);
   }
+
+  if (birdMixer){
+  //  birdMixer.position.x = Math.cos(-40*birdTime*2*Math.PI);
+    
+    //birdMixer.position.z = Math.sin(-20 + 20*birdTime*2*Math.PI);
+    birdTime += 0.01;
+    if (birdTime > 10)
+      birdTime = 0
+    birdMixer.update(0.02);
+  }
+/*
+  if (birdModel) {
+    birdModel.position.x = Math.cos(-40*birdTime*2*Math.PI);
+    
+    birdModel.position.z = Math.sin(-20 + 20*birdTime*2*Math.PI);
+    birdTime += 0.01;
+    if (birdTime > 10)
+      birdTime = 0
+  }
+*/
   renderer.render(scene, camera);
 
   //  renderer.
